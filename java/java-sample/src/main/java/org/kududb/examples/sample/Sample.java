ea2f84224b082fdf2ce36b3b4629318a480a498b
Joe Trite <joetrite@gmail.com>
2017-02-22 17:50:11 -0500

    additional details
    a few more examples for your viewing pleasure.

--- java/java-sample/src/main/java/org/kududb/examples/sample/Sample.java
+++ java/java-sample/src/main/java/org/kududb/examples/sample/Sample.java
@@ -1,9 +1,11 @@
 package org.kududb.examples.sample;
 
 import org.apache.kudu.ColumnSchema;
+import org.apache.kudu.ColumnSchema.ColumnSchemaBuilder;
 import org.apache.kudu.Schema;
 import org.apache.kudu.Type;
 import org.apache.kudu.client.*;
+import org.apache.kudu.client.KuduPredicate.ComparisonOp;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +13,7 @@ import java.util.List;
 public class Sample {
 
   private static final String KUDU_MASTER = System.getProperty(
-      "kuduMaster", "quickstart.cloudera");
+		  "kuduMaster", "quickstart.cloudera");
 
   public static void main(String[] args) {
     System.out.println("-----------------------------------------------");
@@ -20,47 +22,124 @@ public class Sample {
     System.out.println("-----------------------------------------------");
     String tableName = "java_sample-" + System.currentTimeMillis();
     KuduClient client = new KuduClient.KuduClientBuilder(KUDU_MASTER).build();
+    
 
     try {
-      List<ColumnSchema> columns = new ArrayList(2);
-      columns.add(new ColumnSchema.ColumnSchemaBuilder("key", Type.INT32)
+    	
+      //******************************	CREATE KUDU TABLE  ********************************
+      System.out.println("Creating kudu schema for table " + tableName);
+
+      List<ColumnSchema> columns = new ArrayList<ColumnSchema>();
+      columns.add(new ColumnSchema.ColumnSchemaBuilder("cust_num", Type.INT64)
           .key(true)
           .build());
-      columns.add(new ColumnSchema.ColumnSchemaBuilder("value", Type.STRING)
+      columns.add(new ColumnSchema.ColumnSchemaBuilder("cust_name", Type.STRING)
           .build());
-      List<String> rangeKeys = new ArrayList<>();
-      rangeKeys.add("key");
+      columns.add(new ColumnSchema.ColumnSchemaBuilder("cust_phone", Type.STRING)
+              .build());
+      List<String> rangeKeys = new ArrayList<String>();
+      rangeKeys.add("cust_num");
 
+      
+      System.out.println("Submitting create table command for " + tableName);
       Schema schema = new Schema(columns);
-      client.createTable(tableName, schema,
-                         new CreateTableOptions().setRangePartitionColumns(rangeKeys));
+      client.createTable(tableName, schema, new CreateTableOptions()
+    		  .setRangePartitionColumns(rangeKeys)
+    		  .setNumReplicas(1)); 
+
+      System.out.println("Table creation complete for " + tableName);
+
+      
+      //******************************	INSERT DATA  ********************************
+      System.out.println("Inserting row into " + tableName);
 
       KuduTable table = client.openTable(tableName);
       KuduSession session = client.newSession();
-      for (int i = 0; i < 3; i++) {
+      for (long i = 0; i < 3; i++) {
         Insert insert = table.newInsert();
         PartialRow row = insert.getRow();
-        row.addInt(0, i);
-        row.addString(1, "value " + i);
-        session.apply(insert);
-      }
+        row.addLong("cust_num", i); 
+        row.addString("cust_name", "John Doe"); 
+        row.addString("cust_phone", "555-555-5555");
+        
+        System.out.println("Inserting row for cust_num " + i);
 
-      List<String> projectColumns = new ArrayList<>(1);
-      projectColumns.add("value");
+        session.apply(insert);
+        System.out.println("Insert complete");
+
+      }
+            
+      //******************************	SELECT DATA ********************************
+      System.out.println("Selecting data that was just inserted");
+
+      List<String> selectColumns = new ArrayList<String>();
+      selectColumns.add("cust_num");
+      selectColumns.add("cust_phone");
+
+      System.out.println("Defining the column we will use in the selection criteria");
+      ColumnSchema cs = new ColumnSchemaBuilder("cust_num", Type.INT64).encoding(ColumnSchema.Encoding.AUTO_ENCODING).key(true).build();
+      
       KuduScanner scanner = client.newScannerBuilder(table)
-          .setProjectedColumnNames(projectColumns)
+          .setProjectedColumnNames(selectColumns)
+          .addPredicate(KuduPredicate.newComparisonPredicate(cs, ComparisonOp.EQUAL, 1)) //get the row where cust_num == 1
           .build();
+      
       while (scanner.hasMoreRows()) {
         RowResultIterator results = scanner.nextRows();
         while (results.hasNext()) {
           RowResult result = results.next();
-          System.out.println(result.getString(0));
+          System.out.println("cust_num=" + result.getLong(0) + ", cust_phone=" + result.getString(1));
         }
       }
+      
+      System.out.println("Select complete");
+      
+      //******************************	UPSERT DATA  ********************************
+      System.out.println("Upserting rows in " + tableName);
+      for (long i = 0; i < 3; i++) {
+          Upsert upsert = table.newUpsert();
+          PartialRow row = upsert.getRow();
+          row.addLong("cust_num", i); 
+          row.addString("cust_name", "Jane Doe"); 
+          row.addString("cust_phone", "777-777-7777");
+          
+          System.out.println("Upserting row for cust_num " + i);
+
+          session.apply(upsert);
+          System.out.println("Upsert complete");
+
+        }
+              
+      //******************************	SELECT DATA AGAIN  ********************************
+        System.out.println("Selecting data that was just upserted.");
+        
+        selectColumns = new ArrayList<String>();
+        selectColumns.add("cust_num");
+        selectColumns.add("cust_phone");
+
+        System.out.println("Defining the column we will use in the selection criteria");
+        cs = new ColumnSchemaBuilder("cust_num", Type.INT64).encoding(ColumnSchema.Encoding.AUTO_ENCODING).key(true).build();
+        
+        scanner = client.newScannerBuilder(table)
+            .setProjectedColumnNames(selectColumns)
+            .addPredicate(KuduPredicate.newComparisonPredicate(cs, ComparisonOp.EQUAL, 1)) //get the row where cust_num == 1
+            .build();
+        while (scanner.hasMoreRows()) {
+          RowResultIterator results = scanner.nextRows();
+          while (results.hasNext()) {
+            RowResult result = results.next();
+            System.out.println("cust_num=" + result.getLong(0) + ", cust_phone=" + result.getString(1));
+          }
+        }
+        
+        System.out.println("Select complete");
+
     } catch (Exception e) {
       e.printStackTrace();
     } finally {
       try {
+      	//******************************	DELETE KUDU TABLE   ********************************
+    	System.out.println("Deleting table " + tableName);
         client.deleteTable(tableName);
       } catch (Exception e) {
         e.printStackTrace();
@@ -74,4 +153,3 @@ public class Sample {
     }
   }
 }
-
